fd73b11bb41098b433886d6b7a096776a215c0ac
huangkaren <karenhuang@uchicago.edu>
2024-10-18 16:46:54 +0000

    finished

--- dice.rb
+++ dice.rb
@@ -9,49 +9,67 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  # first_die = rand(1..6)
+  # second_die = rand(1..6)
+  # sum = first_die + second_die
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
- 
+  # @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   @rolls = []
 
   2.times do
     die = rand(1..6)
     @rolls.push(die)
   end
-
+ 
   erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  # first_die = rand(1..10)
+  # second_die = rand(1..10)
+  # sum = first_die + second_die
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  # @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+    @rolls.push(die)
+  end
 
     erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  @die = rand(1..20)
+  # @die = rand(1..20)
 	
-	@outcome = "You rolled a #{@die}."
+	# @outcome = "You rolled a #{@die}."
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+    @rolls.push(die)
+  end
 	
 	   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
+  # first_die = rand(1..4)
+	# second_die = rand(1..4)
+  # third_die = rand(1..4)
+  # fourth_die = rand(1..4)
+  # sum = first_die + second_die + third_die + fourth_die
 	
-	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	# @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end
+
    erb(:five_four)
 end
 
