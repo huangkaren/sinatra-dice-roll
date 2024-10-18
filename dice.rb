04fb12988cfb141f34b2ac61675ff2982b5f73fa
huangkaren <karenhuang@uchicago.edu>
2024-10-18 04:06:18 +0000

    erb

--- dice.rb
+++ dice.rb
@@ -8,23 +8,14 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 
-get("/zebra")do
-  "We must add a route for each path we want to support"
-end
-
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server"
-end
-
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six,)
 end
 
 get("/dice/2/10") do
@@ -32,19 +23,17 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+    erb(:two-ten)
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
 	
-	outcome = "You rolled a #{die}."
+	@outcome = "You rolled a #{die}."
 	
-	"<h1>1d20</h1>
-   <p>#{outcome}</p>"
+	   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -54,21 +43,11 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 	
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
+   erb(:five_four)
 end
 
 get("/") do
-  "<h1>Dice Roll </h1>
-  <ul>
-    <li><a href =\"/dice/2/6\"> Roll two 6-sided die </li>
-
-    <li><a href = \"dice/2/10\"> Roll two 10-sided die</li>
-
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>"
+  erb(:elephant)
 end
